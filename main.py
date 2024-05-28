714b725427c710a321d295873a70f181b7ab990f
TanThanhLe <tanthanhcr95@gmail.com>
2024-05-29 00:01:03 +0700

    Python __repr__

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print("Hello world")
+from person import Person
+
+# Python __repr__
+
+person = Person('John', 'Doe', 25)
+
+print(f"repr: {repr(person)}")  # call __repr__
+print(f"str: {str(person)}")    # call __repr__ if __str__ isn't defined
